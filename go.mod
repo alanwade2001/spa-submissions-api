9d91052035c2cab70f0cf46fcb013a3086ced345
alanwade2001 <alanwade2001@yahoo.com>
2021-03-20 18:27:36 +0000

    changing to common mongo

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/alanwade2001/spa-submissions-api
 go 1.16
 
 require (
+	github.com/alanwade2001/spa-common v1.1.2 // indirect
 	github.com/gin-gonic/gin v1.6.3
 	github.com/go-resty/resty/v2 v2.5.0
 	github.com/google/wire v0.5.0
--- repositories/Mongo.go
+++ repositories/Mongo.go
@@ -1,9 +1,7 @@
 package repositories
 
 import (
-	"context"
 	"errors"
-	"fmt"
 	"reflect"
 	"time"
 
@@ -14,42 +12,34 @@ import (
 	"go.mongodb.org/mongo-driver/bson/bsoncodec"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+
+	mgo "github.com/alanwade2001/spa-common/mongo"
 
 	"k8s.io/klog/v2"
 )
 
-// MongoService s
-type MongoService struct {
+// MongoRepository s
+type MongoRepository struct {
+	service *mgo.MongoService
 }
 
 // NewMongoService s
 func NewMongoService() types.RepositoryAPI {
-	return &MongoService{}
+	return &MongoRepository{}
 }
 
-// MongoConnection s
-type MongoConnection struct {
-	client *mongo.Client
-	ctx    context.Context
-	cancel context.CancelFunc
-}
+func (ms *MongoRepository) GetService() *mgo.MongoService {
 
-// Disconnect f
-func (mc *MongoConnection) Disconnect() {
-	mc.cancel()
-	mc.client.Disconnect(mc.ctx)
-}
+	if ms.service != nil {
+		return ms.service
+	}
 
-// Connect f
-func (ms MongoService) connect() MongoConnection {
-	username := viper.GetString("MONGODB_USER")
-	klog.Infof("mongo user: [%s]", username)
-	password := viper.GetString("MONGODB_PASSWORD")
 	uriTemplate := viper.GetString("MONGODB_URI_TEMPLATE")
-	klog.Infof("uriTemplate: [%s]", uriTemplate)
-
-	connectionURI := fmt.Sprintf(uriTemplate, username, password)
+	username := viper.GetString("MONGODB_USER")
+	password := viper.GetString("MONGODB_PASSWORD")
+	connectTimeout := viper.GetDuration("MONGODB_TIMEOUT") * time.Second
+	database := viper.GetString("MONGODB_DATABASE")
+	collection := viper.GetString("MONGODB_COLLECTION")
 
 	structcodec, _ := bsoncodec.NewStructCodec(bsoncodec.JSONFallbackStructTagParser)
 	reg := bson.NewRegistryBuilder().
@@ -57,43 +47,79 @@ func (ms MongoService) connect() MongoConnection {
 		RegisterTypeDecoder(reflect.TypeOf(submission.SubmissionModel{}), structcodec).
 		Build()
 
-	client, err := mongo.NewClient(options.Client().ApplyURI(connectionURI).SetRegistry(reg))
-	if err != nil {
-		klog.Warningf("Failed to create client: %v", err)
-	}
+	service := mgo.NewMongoService(uriTemplate, username, password, database, collection, connectTimeout, reg)
 
-	connectTimeout := viper.GetDuration("MONGODB_TIMEOUT") * time.Second
-	ctx, cancel := context.WithTimeout(context.Background(), connectTimeout)
+	ms.service = service
 
-	err = client.Connect(ctx)
-	if err != nil {
-		klog.Warningf("Failed to connect to cluster: %v", err)
-	}
-
-	// Force a connection to verify our connection string
-	err = client.Ping(ctx, nil)
-	if err != nil {
-		klog.Warningf("Failed to ping cluster: %v", err)
-	}
-
-	klog.Infof("Connected to MongoDB!")
-
-	return MongoConnection{client, ctx, cancel}
+	return ms.service
 }
 
-func (ms MongoService) getCollection(connection MongoConnection) *mongo.Collection {
-	database := viper.GetString("MONGODB_DATABASE")
-	return connection.client.Database(database).Collection("Submissions")
-}
+// MongoConnection s
+// type MongoConnection struct {
+// 	client *mongo.Client
+// 	ctx    context.Context
+// 	cancel context.CancelFunc
+// }
+
+// // Disconnect f
+// func (mc *MongoConnection) Disconnect() {
+// 	mc.cancel()
+// 	mc.client.Disconnect(mc.ctx)
+// }
+
+// Connect f
+// func (ms MongoRepository) connect() MongoConnection {
+// 	username := viper.GetString("MONGODB_USER")
+// 	klog.Infof("mongo user: [%s]", username)
+// 	password := viper.GetString("MONGODB_PASSWORD")
+// 	uriTemplate := viper.GetString("MONGODB_URI_TEMPLATE")
+// 	klog.Infof("uriTemplate: [%s]", uriTemplate)
+
+// 	connectionURI := fmt.Sprintf(uriTemplate, username, password)
+
+// 	structcodec, _ := bsoncodec.NewStructCodec(bsoncodec.JSONFallbackStructTagParser)
+// 	reg := bson.NewRegistryBuilder().
+// 		RegisterTypeEncoder(reflect.TypeOf(submission.SubmissionModel{}), structcodec).
+// 		RegisterTypeDecoder(reflect.TypeOf(submission.SubmissionModel{}), structcodec).
+// 		Build()
+
+// 	client, err := mongo.NewClient(options.Client().ApplyURI(connectionURI).SetRegistry(reg))
+// 	if err != nil {
+// 		klog.Warningf("Failed to create client: %v", err)
+// 	}
+
+// 	connectTimeout := viper.GetDuration("MONGODB_TIMEOUT") * time.Second
+// 	ctx, cancel := context.WithTimeout(context.Background(), connectTimeout)
+
+// 	err = client.Connect(ctx)
+// 	if err != nil {
+// 		klog.Warningf("Failed to connect to cluster: %v", err)
+// 	}
+
+// 	// Force a connection to verify our connection string
+// 	err = client.Ping(ctx, nil)
+// 	if err != nil {
+// 		klog.Warningf("Failed to ping cluster: %v", err)
+// 	}
+
+// 	klog.Infof("Connected to MongoDB!")
+
+// 	return MongoConnection{client, ctx, cancel}
+// }
+
+// func (ms MongoRepository) getCollection(connection MongoConnection) *mongo.Collection {
+// 	database := viper.GetString("MONGODB_DATABASE")
+// 	return connection.client.Database(database).Collection("Submissions")
+// }
 
 // CreateSubmission f
-func (ms MongoService) CreateSubmission(submission *submission.SubmissionModel) (*submission.SubmissionModel, error) {
-	connection := ms.connect()
+func (ms MongoRepository) CreateSubmission(submission *submission.SubmissionModel) (*submission.SubmissionModel, error) {
+	connection := ms.GetService().Connect()
 	defer connection.Disconnect()
 
 	submission.Id = primitive.NewObjectID().Hex()
 
-	result, err := ms.getCollection(connection).InsertOne(connection.ctx, submission)
+	result, err := ms.GetService().GetCollection(connection).InsertOne(connection.Ctx, submission)
 
 	if err != nil {
 		klog.Warningf("Could not create Submission: %v", err)
@@ -106,15 +132,15 @@ func (ms MongoService) CreateSubmission(submission *submission.SubmissionModel) 
 }
 
 // GetSubmission f
-func (ms MongoService) GetSubmission(ID string) (sub *submission.SubmissionModel, err error) {
-	connection := ms.connect()
+func (ms MongoRepository) GetSubmission(ID string) (sub *submission.SubmissionModel, err error) {
+	connection := ms.GetService().Connect()
 	defer connection.Disconnect()
 
 	sub = new(submission.SubmissionModel)
 
 	filter := bson.M{"_id": ID}
 
-	if err := ms.getCollection(connection).FindOne(connection.ctx, filter).Decode(sub); err != nil {
+	if err := ms.GetService().GetCollection(connection).FindOne(connection.Ctx, filter).Decode(sub); err != nil {
 		if errors.Is(err, mongo.ErrNoDocuments) {
 			return nil, nil
 		}
@@ -128,8 +154,8 @@ func (ms MongoService) GetSubmission(ID string) (sub *submission.SubmissionModel
 }
 
 // GetSubmissions f
-func (ms MongoService) GetSubmissions() ([]*submission.SubmissionModel, error) {
-	connection := ms.connect()
+func (ms MongoRepository) GetSubmissions() ([]*submission.SubmissionModel, error) {
+	connection := ms.GetService().Connect()
 	defer connection.Disconnect()
 
 	var cursor *mongo.Cursor
@@ -137,7 +163,7 @@ func (ms MongoService) GetSubmissions() ([]*submission.SubmissionModel, error) {
 	submissions := []*submission.SubmissionModel{}
 
 	filter := bson.M{}
-	if cursor, err = ms.getCollection(connection).Find(connection.ctx, filter); err != nil {
+	if cursor, err = ms.GetService().GetCollection(connection).Find(connection.Ctx, filter); err != nil {
 		if errors.Is(err, mongo.ErrNoDocuments) {
 			return submissions, nil
 		}
@@ -145,7 +171,7 @@ func (ms MongoService) GetSubmissions() ([]*submission.SubmissionModel, error) {
 		return nil, err
 	}
 
-	if err = cursor.All(connection.ctx, &submissions); err != nil {
+	if err = cursor.All(connection.Ctx, &submissions); err != nil {
 		return nil, err
 	}
 
